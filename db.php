9bb23e18fb77589baee74286df7cef6754f08146
Del Puerto <bdelpuerto@dxc.com>
2019-08-26 17:15:27 +0800

    changed db

--- db.php
+++ db.php
@@ -2,14 +2,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = 'database-webserver.clotilhxolet.us-east-2.rds.amazonaws.com'; // <<<<<<<<<< replace IP if needed
-$port = '3306';
+//$host = 'database-webserver.clotilhxolet.us-east-2.rds.amazonaws.com'; // <<<<<<<<<< replace IP if needed
+$host = 'mysql';
+//$port = '3306';
 $db   = 'dxc';
-$user = 'admin';
-$pass = 'admin123';
+$user = 'user';
+$pass = 'pass';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+//$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
