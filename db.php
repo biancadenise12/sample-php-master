08bca1dcf4300b6879634626bcb52ff4b048e8c9
Bianca del Puerto <biancadenise12@users.noreply.github.com>
2019-09-05 11:15:19 +0800

    update db.php

--- db.php
+++ db.php
@@ -2,14 +2,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = 'database-webserver.cmc23fguffkc.us-east-1.rds.amazonaws.com'; //if using RDS, get the Endpoint; else if using mysql container, get the container name or ID
-$port = '3306'; //include this line if using RDS
+//$host = 'database-webserver.cmc23fguffkc.us-east-1.rds.amazonaws.com'; //if using RDS, get the Endpoint; else if using mysql container, get the container name or ID
+//$port = '3306'; //include this line if using RDS
+$host = 'mysql'
 $db   = 'dxc';
-$user = 'admin';
-$pass = 'admin123';
+$user = 'user';
+$pass = 'pass';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+//$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
